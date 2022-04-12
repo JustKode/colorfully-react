a7d26b9df1e0a85418b043263df44bd1b775b345
juicyorange <30497935+juicyorange@users.noreply.github.com>
2022-04-12 18:17:51 +0900

    Revert "Develop/front "

--- frontend/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  env: { browser: true, es6: true, node: true },
-  extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
-  rules: { 'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }] },
-};
