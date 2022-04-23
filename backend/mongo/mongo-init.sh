3cb75aac98de90d8c7164d5011b971a3dda3151c
justkode <sobu0715@gmail.com>
2022-04-24 03:54:20 +0900

    fix: mongodb Setting

---
+++ backend/mongo/mongo-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+mongo <<EOF
+use $MONGO_INITDB_DATABASE
+db.createUser({
+  user:  '$MONGO_INITDB_ROOT_USERNAME',
+  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+  roles: [{
+    role: 'readWrite',
+    db: '$MONGO_INITDB_DATABASE'
+  }]
+});
+db.createCollection('dc_schema');
+EOF
