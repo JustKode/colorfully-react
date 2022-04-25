d03adee4e5289952088162261a697bbd1ae47c83
justkode <sobu0715@gmail.com>
2022-04-25 21:15:46 +0900

    feat: Get Color By ObjectId API

--- backend/util/mongo.py
+++ backend/util/mongo.py
@@ -1,5 +1,5 @@
 from pymongo import mongo_client
-from bson import BSON
+from bson import BSON, objectid
 import os
 
 host = os.getenv("HOST", "localhost")
@@ -13,3 +13,7 @@ def set_schema(json):
     db = connection.get_default_database()
     result = {"id": str(db.get_collection('db_schema').insert_one(json).inserted_id)}
     return result
+
+def get_schema(page_id):
+    db = connection.get_default_database()
+    return db.get_collection('db_schema').find_one(objectid.ObjectId(page_id))
