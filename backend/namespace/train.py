9a0ad52eccae194a558f3e7b413f186a29638608
justkode <sobu0715@gmail.com>
2022-05-10 01:04:16 +0900

    fix: remove schema + add groups

--- backend/namespace/train.py
+++ backend/namespace/train.py
@@ -1,31 +1,36 @@
 from flask import request
 from flask_restx import Resource, Namespace, fields
-from json import dumps
+from json import loads
 from requests import post
 from .data import color_data_fields
 from util.mongo import get_schema, post_data
 from util.enum import EnumUtil
+
+enum_util = EnumUtil()
 
 Train = Namespace(
     name="Train",
     description="학습을 수행 합니다."
 )
 
-@Train.route('/<string:page_id>')
-@Train.doc(params={'page_id': 'Page Initialize시 반환 받은 ID 입니다.'})
-class TrainPage(Resource):
-    @Train.response(200, "반환 성공", color_data_fields)
-    @Train.response(404, "아이디 조회 실패")
-    def get(self, page_id):
-        """현재까지의 데이터로 학습한 결과를 반환 합니다."""
-        schema = get_schema(page_id)
+result_fields = Train.model("학습 결과", {key: fields.List(fields.Nested(color_data_fields)) for key in enum_util.get_adjectives()})
 
-        if schema is None:
-            return {"message": "page not found"}, 404
+@Train.route('/<string:group_id>')
+@Train.doc(params={'group_id': 'group_adjective.json에 명시한 Group ID 입니다.'})
+class TrainPage(Resource):
+    @Train.response(200, "반환 성공", result_fields)
+    @Train.response(404, "아이디 조회 실패")
+    def get(self, group_id):
+        """현재까지의 데이터로 학습한 결과를 반환 합니다."""
+        with open('./group_adjective.json', 'r') as f:
+            groups = loads(f.read())
+
+        if group_id not in groups:
+            return {"message": "group not found"}, 404
         else:
-            return {
-                "mainBackGroup": "#123456",
-                "mainGroup": "#123456",
-                "subGroup": "#123456",
-                "point": "#123456"
-            }
+            return {key: [{
+                        "mainBackGroup": "#123456",
+                        "mainGroup": "#123456",
+                        "subGroup": "#123456",
+                        "point": "#123456"
+                    } for _ in range(3)] for key in enum_util.get_adjectives()}
