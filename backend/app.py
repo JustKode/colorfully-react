ff41bcfbf5b646689bfee04f62193469b30e17a9
justkode <sobu0715@gmail.com>
2022-05-09 19:42:17 +0900

    feat: add namespace Train

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask_restx import Api
 from namespace.init import Init
 from namespace.data import Data
+from namespace.train import Train
 
 app = Flask(__name__)
 CORS(app)
@@ -19,6 +20,7 @@ api = Api(
 
 api.add_namespace(Init, '/init')
 api.add_namespace(Data, '/data')
+api.add_namespace(Train, '/train')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8000)
